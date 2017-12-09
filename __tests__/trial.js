124c1d6783b154a6f4e9120aa61e9f7d9e14db42
Jason Borer <borerjason@gmail.com>
2017-12-09 11:37:28 -0800

    add jest sample test

---
+++ __tests__/trial.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+
+test('Output true when two numbers are equal', () => {
+  
+  expect(3).toBe(3);
+});
+
+/* eslint-enable */
---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express =  require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Listing on port ${PORT}`));
