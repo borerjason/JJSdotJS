ffe5262ae8e0d21dd69056951b208a594bed1403
Jason Borer <borerjason@gmail.com>
2017-12-11 21:23:34 -0800

    update test suite

--- test/index.js
+++ test/index.js
@@ -5,13 +5,6 @@ const test = require('tape');
 test('timing test', function(t) {
   // t.plan(2);
   t.equal(typeof Date.now, 'function');
-  
-
-  // const start = Date.now();
-
-  // setTimeout(function () {
-  //   t.equal(Date.now() - start, 100);
-  // }, 100);
 
   t.end();
 });
