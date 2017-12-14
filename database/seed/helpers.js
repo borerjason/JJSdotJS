2b4e945c57b202f3bd3014b89f4c68baba5c9f2a
Jason Borer <borerjason@gmail.com>
2017-12-14 14:39:26 -0800

    db seed function v1

--- database/seed/helpers.js
+++ database/seed/helpers.js
@@ -1,7 +1,6 @@
 
 const dates = ['1/1/2018', '1/8/2017', '1/15/2017', '1/22/2017', '1/29/2017', '2/5/2017', '2/12/2017', '2/19/2017', '2/26/2017', '3/5/2017', '3/12/2017', '3/19/2017'];
 
-475400	890907	1365907	1899907	2374907	2849907	3442907	3976907	4569907	5044907	5459907	5993907
 const controlDate = (count) => {
   if (count < 475400) {
     return dates[0];
@@ -46,40 +45,41 @@ const experimentDate = (count) => {
   if (count < 475400) {
     return dates[0];
   }
-  if (count < 982598) {
+  if (count < 950693) {
     return dates[1];
   }
-  if (count < 1508642) {
+  if (count < 1484693) {
     return dates[2];
   }
-  if (count < 1661574) {
+  if (count < 1899693) {
     return dates[3];
   }
-  if (count < 1758763) {
+  if (count < 2196693) {
     return dates[4];
   }
-  if (count < 1424163) {
+  if (count < 2374693) {
     return dates[5];
   }
-  if (count < 2040490) {
+  if (count < 2671693) {
     return dates[6];
   }
-  if (count < 2801840) {
+  if (count < 3027693) {
     return dates[7];
   }
-  if (count < 2634490) {
+  if (count < 3324693) {
     return dates[8];
   }
-  if (count < 2864837) {
+  if (count < 3561693) {
     return dates[9];
   }
-  if (count < 3365472) {
+  if (count < 3798693) {
     return dates[10];
   }
-  if (count < 2803402) {
+  if (count < 4006693) {
     return dates[11];
   }
   return null;
-}
+};
 
 module.exports.controlDate = controlDate;
+module.exports.experimentDate = experimentDate;
--- database/seed/seed.js
+++ database/seed/seed.js
@@ -24,39 +24,65 @@ const eventTypes = ['click', 'like', 'view'];
 
 let count = 0;
 let item = 0;
-let start = 0;
-let max = 500;
+let max = 200;
+
 const insertEvent = 'INSERT INTO events (id, user_id, experimentgroup, item_id, itemtype, eventtype, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?)';
 
-const controlGroup = (date, itemNum, startNum, maxNum) => {
+const seedEvents = (date, itemNum, startNum, maxNum, group) => {
   const arr = [];
-  for (; start < max; start += 2) {
-    arr.push({ query: insertEvent, params: [uniqid(), start, 1, uniqid(), itemTypes[item], eventTypes[item], date] });
+  let i = startNum;
+  for (; i < max; i += 2) {
+    arr.push({ query: insertEvent, params: [uniqid(), i, group, uniqid(), itemTypes[item], eventTypes[item], date] });
   }
 
-  client.client.batch(arr, { prepare: true }).then((success) => {
+  client.client.batch(arr, { prepare: true }).then(() => {
     count += arr.length;
     if (itemNum === 0) {
       item = 1;
-    } else if ( item === 1 ) {
+    } else if (item === 1) {
       item = 2;
     } else {
       item = 0;
     }
 
-    if (maxNum === 10000) {
-      max = 500;
-      start = 0;
-    } else {
-      max += 500;
-    }
-    const week = controlDate(count);
+    if (group === experimentGroups.control) {
+      const week = controlDate(count);
 
-    if (count < 5993907) {
-      controlGroup(week, item, start, max);
+      if (maxNum === 10000) {
+        max = 200;
+        i = 0;
+      } else {
+        max += 200;
+      }
+
+      if (count < 5993907) {
+        seedEvents(week, item, i, max, experimentGroups.control);
+      } else {
+        count = 0;
+      }
+      console.log(count);
     }
-    console.log(count);
+
+    if (group === experimentGroups.experiment) {
+      if (maxNum === 10000) {
+        max = 200;
+        i = 1;
+      } else {
+        max += 200;
+      }
+
+      const week = experimentDate(count);
+      if (count < 4006693) {
+        seedEvents(week, item, i, max, experimentGroups.experiment);
+      } else {
+        count = 0;
+      }
+      console.log(count);
+    }
   });
 };
 
-controlGroup('1/1/2018', item, start, max);
+// Uncomment and run file to seed database
+// seedEvents('1/1/2018', item, 0, max, experimentGroups.control);
+// seedEvents('1/1/2018', item, 1, max, experimentGroups.experiment);
+
