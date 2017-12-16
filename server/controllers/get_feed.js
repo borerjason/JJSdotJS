37c6748709c20d5ea1617cb2efad3e45a062d1de
Jason Borer <borerjason@gmail.com>
2017-12-16 15:46:25 -0800

    send dummy posts and averts to user

--- server/controllers/get_feed.js
+++ server/controllers/get_feed.js
@@ -4,30 +4,31 @@ const MockAdapter = require('axios-mock-adapter');
 const dummy = require('../../dummy_data');
 
 // ------ mock for service testing -------
-console.log('DUMMY', dummy);
-const mock = new MockAdapter(axios, { delayResponse: 1000 });
+
+const mock = new MockAdapter(axios, { delayResponse: 25 });
 mock
   .onGet('/users/0/feed').reply(200, { feed: dummy.feed })
   .onGet('/adverts?userId=0').reply(200, { adverts: dummy.adverts });
 
 // ---------------------------------------
 
-const feed = ({ body }, res) => {
+const feed = (req, res) => {
+  console.log('Triggered!');
   // check if they are in experimental group
   // serve feed/home page to the user
-  const userId = body.userID;
+  // const userId = body.userID;
+  const userId = 0;
   const contentFeed = axios.get(`/users/${userId}/feed`);
   const adverts = axios.get(`/adverts?userId=${userId}`);
 
   Promise.all([contentFeed, adverts])
     .then((response) => {
-      console.log(response);
-      res.send('got your results!');
+      console.log('promises resolved');
+      res.send(response[0].data.feed.concat(response[1].data.adverts));
     })
     .catch((err) => {
       console.log(err);
     });
- 
 };
 
 module.exports.feed = feed;
