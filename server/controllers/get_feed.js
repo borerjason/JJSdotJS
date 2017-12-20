2c6f40ce2be15a3ab11f20a938e3a76164a127cd
Jason Borer <borerjason@gmail.com>
2017-12-20 14:16:35 -0800

    add redis functionality to get_feed

--- server/controllers/get_feed.js
+++ server/controllers/get_feed.js
@@ -1,22 +1,45 @@
 const axios = require('axios');
-
-require('../../dummy_data/mocks');
+const redis = require('redis');
+require('dotenv').config();
 
 const prepareFeed = require('../helpers/prepare_feed');
 
+// process.env.RED_PORT, process.env.RED_HOST)
+const client = redis.createClient();
+
+client.on('connect', function (res, err) {
+  console.log('connected to redis');
+});
+
+require('../../dummy_data/mocks');
+
+
+
 const feed = (req, res) => {
   console.log('Triggered!');
+/*
+   - check to see if key is stored in red cache
+      - if yes serve back to client
+      - if no prepare new feed
+       - send feed to client
+       - add to red cache
+*/
+  const userId = 0;
+  // if doesn't have it reply === null
+
+  client.get('userId', (err, reply) => {
+    console.log('Redis Get Request', JSON.parse(reply));
+  });
   // check if they are in experimental group
   // serve feed/home page to the user
-  // const userId = body.userID;
-  const userId = 0;
   const contentFeed = axios.get(`/users/${userId}/feed`);
   const adverts = axios.get(`/adverts?userId=${userId}`);
-
+  
   Promise.all([contentFeed, adverts])
     .then((response) => {
       console.log('promises resolved');
       const feedFinal = prepareFeed(3, response[0].data.feed, response[1].data.adverts);
+      client.set('userId', JSON.stringify(feedFinal));
       res.send(feedFinal);
     })
     .catch((err) => {
