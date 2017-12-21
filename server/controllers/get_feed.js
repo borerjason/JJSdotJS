767cbfc08ee487c466a7239da62afbdfc8d99bb8
Jason Borer <borerjason@gmail.com>
2017-12-20 19:26:54 -0800

    add check group functionality to get feed

--- server/controllers/get_feed.js
+++ server/controllers/get_feed.js
@@ -1,4 +1,3 @@
-const axios = require('axios');
 const redis = require('redis');
 require('dotenv').config();
 
@@ -14,10 +13,11 @@ client.on('connect', function (res, err) {
 require('../../dummy_data/mocks');
 
 module.exports = (req, res) => {
-  const userId = 5;
+  const userId = Math.floor(Math.random() * 10000);
+  const groupStr = `${userId}Group`;
+
   client.get(userId, (err, reply) => {
     if (reply) {
-      console.log('User ID: ', userId);
       const userFeed = JSON.parse(reply);
       res.send(200, userFeed);
       // in order to update add this part of the function to a queue
@@ -26,14 +26,25 @@ module.exports = (req, res) => {
       //     client.set(userId, JSON.stringify(result));
       //   });
     } else {
-      console.log('User ID: ', userId);
-      fetchFeed(userId)
-        .then((result) => {
-          console.log('Result', result);
-          res.send(200, result);
-          client.set(userId, JSON.stringify(result));
-        });
+      let group = null;
+      // Check if user is assigned to experiment group and retrive it or assign it
+      client.get(groupStr, (err, reply) => {
+        if (reply) {
+          group = reply;
+        } else {
+          const num = Math.floor(Math.random() * 2);
+          num === 0 ? group = 1 : group = 3;
+          // assign group number to user
+          client.set(groupStr, group);
+        }
+        // get feed for user who is not in the cache
+        fetchFeed(userId, group)
+          .then((result) => {
+            console.log('RESULT', group, result);
+            res.send(200, result);
+            client.set(userId, JSON.stringify(result));
+          });
+      });
     }
   });
 };
-
--- server/helpers/fetch_feed.js
+++ server/helpers/fetch_feed.js
@@ -1,20 +1,19 @@
 const axios = require('axios');
 const prepareFeed = require('./prepare_feed');
 
-module.exports = (userId) => {
-
-  return new Promise((resolve, reject) => {
+module.exports = (userId, group) => (
+  new Promise((resolve, reject) => {
     const contentFeed = axios.get(`/users/${userId}/feed`);
     // change back to correct from app plan when ready to deploy
     const adverts = axios.get(`/adverts/${userId}/feed`);
 
     Promise.all([contentFeed, adverts])
       .then((response) => {
-        resolve(prepareFeed(3, response[0].data.feed, response[1].data.adverts));
+        resolve(prepareFeed(group, response[0].data.feed, response[1].data.adverts));
       })
       .catch((err) => {
         console.log(err);
         reject(err);
       });
-  });
-};
+  })
+);
