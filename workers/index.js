1974bb292d5f38d22a8ad5acdf9a01cd5eb544ce
Jason Borer <borerjason@gmail.com>
2018-01-02 15:03:52 -0800

    update kue host

--- workers/index.js
+++ workers/index.js
@@ -1,7 +1,7 @@
 const kue = require('kue');
 const cacheFeed = require('./cache_feed.js');
 
-const queue = kue.createQueue({ jobEvents: false });
+const queue = kue.createQueue({ jobEvents: false, redis: { host: process.env.RED_HOST } });
 
 queue.process('cacheFeed', (job, done) => {
   cacheFeed(job.data.userId, done);
