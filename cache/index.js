dea186caf2bd47d84238f2fe227b815e43dac03f
Jason Borer <borerjason@gmail.com>
2018-01-02 11:10:35 -0800

    update redis settings for deployment

--- cache/index.js
+++ cache/index.js
@@ -1,8 +1,9 @@
 const redis = require('redis');
 
-const client = redis.createClient(process.env.RED_PORT, 'localhost');
-// for deploy
-// const client = redis.createClient(process.env.RED_PORT, process.env.RED_HOST);
+// --- for local testing ---
+// const client = redis.createClient(process.env.RED_PORT, 'localhost');
+// --- for deploy ---
+const client = redis.createClient(process.env.RED_PORT, process.env.RED_HOST);
 
 client.on('connect', () => {
   console.log('connected to redis');
