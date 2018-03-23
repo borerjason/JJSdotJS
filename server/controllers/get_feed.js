696c8ba6c9b3696a1c14fe380764f1a8b63e9594
Jason Borer <borerjason@gmail.com>
2018-03-23 10:47:54 -0700

    update content utilized

--- server/controllers/get_feed.js
+++ server/controllers/get_feed.js
@@ -7,17 +7,22 @@ require('dotenv').config();
 require('../../dummy_data/mocks');
 
 module.exports = (req, res) => {
-  console.log('request', req);
   const userId = Math.floor(Math.random() * 5);
   client.get(userId, (err, reply) => {
     if (reply) {
       const userFeed = JSON.parse(reply);
-      res.send(200, userFeed);
+      const startIndex = userFeed[userFeed.length - 1];
+      const endIndex = userFeed[startIndex + 10] ? startIndex + 10 : userFeed.length - 1;
+      const feedSection = userFeed.slice(startIndex, endIndex);
+      res.send(200, feedSection);
+      if (endIndex - startIndex < 10) {
+        client.del(userId);
+      }
       queue.create('cacheFeed', {
         userId,
       }).save();
     } else {
-      const group = fetchGroup(userId);  
+      const group = fetchGroup(userId);
       fetchFeed(userId, group)
         .then((result) => {
           res.send(200, result);
